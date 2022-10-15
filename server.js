fb102ab893aa75dbc81dd76ee2a7619b4a059be9
jsong73 <jessicajinsong@gmail.com>
2022-10-15 02:09:44 -0400

    adding to my server.js file

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
+
+const notes = require("./db/notes.json")
+
 //use preconfigured port ; if none go to default specified 3001 port
 const PORT = process.env.PORT || 3001;
 
+//calling the express library
 const app = express();
 
 //For POST and PUT requests for sending data to the server
@@ -14,10 +19,41 @@ app.get("/", (req, res) =>
  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
+// GET request for notes
+app.get("/api/notes", (req, res) => {
+res.status(200).json(notes)
+});
+
+//POST request to add notes
+app.post("/api/notes", (req, res) => {
+console.log ("note has been added")
+});
+
+const newNote = {
+
+
+}
+
+
+//converts new note data to string 
+const noteString = JSON.stringify(newNote);
+
+fs.readFile(notes, "utf-8", (err, data) => {
+const parsedNotes = JSON.parse(data)
+parsedNotes.push(newNote);
+
+//parameters to format array spacing 
+const stringifiedNotes = JSON.stringify(parsedNotes, null, 4);
+
+fs.writeFile(notes, stringifiedNotes, (err) =>{
+console.log(err);
+});
+});
+
 
 //allowing express access to the public folder 
 app.use(express.static("public"));
 
-
+//listens to PORT
 app.listen(PORT, () =>
-console.log(`Listening for requests on port ${PORT}!`));
+console.log(`App listening at http://localhost:${PORT}!`));
